8df1059b575210d2a3689431c472bbb3cb83da85
Meimmmm <mei.kuri624@gmail.com>
2026-09-26 00:22:46 +0930

    Fix OSM tile URL (remove deprecated subdomain)

--- library-map-frontend/src/components/MapView.tsx
+++ library-map-frontend/src/components/MapView.tsx
@@ -121,10 +121,11 @@ function MapView({
         zoom={13}
         scrollWheelZoom={true}
       >
-        <TileLayer
-          attribution="&copy; OpenStreetMap contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        maxZoom={19}
+      />
 
         <MyLocationControl
           onStart={() => setLocationError(null)}
